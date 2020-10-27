2283827010d07e569f490bd4f202c2e11ac60067
edsonfullstack <edson.fullstack@gmail.com>
2020-10-27 00:29:55 -0300

    0.0.8

--- src/test/java/com/beerhouse/resources/BeerResourceTest.java
+++ src/test/java/com/beerhouse/resources/BeerResourceTest.java
@@ -1,13 +1,16 @@
 package com.beerhouse.resources;
 
+import com.beerhouse.repository.BeerRepository;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
